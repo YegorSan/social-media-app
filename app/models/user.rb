6a4e47a6fff557a68a89abe84b9e27206a793d33
Yegor San <egorgolovan89@gmail.com>
2016-11-08 19:28:27 +0200

    Added userlist, simplest friendship model configured views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
     has_many :posts
     
-    
-    
+    has_many :friendships
+    has_many :friends, :through => :friendships
+    has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+    has_many :inverse_friends, :through => :inverse_friendships, :source => :user
     def fullname
        
         first_name + " " + last_name
